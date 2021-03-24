f4b944b70c01c45e88f85eb6e39909feeb69bc2b
RolS73 <73384621+RolS73@users.noreply.github.com>
2021-03-24 15:15:45 +0100

    Initial Solution

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,37 @@
 package com.codecool.chessopen;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        HashMap<String, Integer> competitorScoresMap = new HashMap<>();
+        List<String> result = new ArrayList<>();
+
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(fileName), "UTF-8");
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] lineData = line.split(",");
+                int competitorScoreSum = Integer.parseInt(lineData[1]) + Integer.parseInt(lineData[2]) + Integer.parseInt(lineData[3]) +
+                        Integer.parseInt(lineData[4]) + Integer.parseInt(lineData[5]);
+                        competitorScoresMap.put(lineData[0], competitorScoreSum);
+            }
+        } catch (IOException ex) {
+            System.out.println("File not found!");
+        }
+
+        competitorScoresMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(stringIntegerEntry -> result.add(stringIntegerEntry.getKey()));
+        return result;
     }
 
 }
